b48657e482c9d68404a144cbeac14ae190bbb745
Rene Dohan <dohan.rene@gmail.com>
2022-08-01 14:19:06 -0500

    no message

--- library/src/main/java/renetik/android/preset/CSPresetStoreItemProperty.kt
+++ library/src/main/java/renetik/android/preset/CSPresetStoreItemProperty.kt
@@ -7,7 +7,6 @@ import renetik.android.event.common.CSModel
 import renetik.android.event.paused
 import renetik.android.event.property.CSProperty
 import renetik.android.event.registration.register
-import renetik.android.event.registration.paused
 import renetik.android.json.obj.getValue
 import renetik.android.preset.property.CSPresetKeyData
 import renetik.android.store.CSStore
@@ -15,53 +14,49 @@ import renetik.android.store.CSStore
 class CSPresetStoreItemProperty<PresetItem : CSPresetItem,
         PresetList : CSPresetItemList<PresetItem>>(
     override val preset: CSPreset<PresetItem, PresetList>,
-    private val parentStore: CSStore,
+    private val store: CSStore,
     val getDefault: () -> PresetItem
 ) : CSModel(preset), CSProperty<PresetItem>, CSPresetKeyData {
 
     override val key = "${preset.id} current"
     override fun saveTo(store: CSStore) = store.set(key, value.toId())
-    private var _value: PresetItem = loadValue()
-    private fun loadValue() =
-        parentStore.getValue(key, preset.list.items) ?: getDefault()
+    private var loadedValue: PresetItem = loadValue()
+    private fun loadValue() = store.getValue(key, preset.list.items) ?: getDefault()
 
     private val eventChange = event<PresetItem>()
 
-    private val parentStoreLoadedRegistration =
-        register(parentStore.eventLoaded.listen { onParentStoreLoaded() })
+    init {
+        register(store.eventLoaded.listen { onParentStoreLoaded() })
+    }
 
     private fun onParentStoreLoaded() {
         if (preset.isFollowStore.isFalse)
             parentStoreLoadedIsFollowStoreFalseSaveToParentStore()
         else {
             val newValue = loadValue()
-            if (_value == newValue) return
-            _value = newValue
-//            parentStoreLoadedRegistration.paused {
-                eventChange.fire(newValue)
-//            }
+            if (loadedValue == newValue) return
+            loadedValue = newValue
+            eventChange.fire(newValue)
         }
     }
 
     private fun parentStoreLoadedIsFollowStoreFalseSaveToParentStore() =
-        parentStore.eventChanged.paused { saveTo(parentStore) }
+        store.eventChanged.paused { saveTo(store) }
 
     override fun value(newValue: PresetItem, fire: Boolean) {
-        if (_value == newValue) return
-        _value = newValue
-//        parentStoreLoadedRegistration.paused {
-            if (fire) eventChange.fire(newValue)
-            preset.reload(newValue)
-            saveTo(parentStore)
-//        }
+        if (loadedValue == newValue) return
+        loadedValue = newValue
+        if (fire) eventChange.fire(newValue)
+        preset.reload(newValue)
+        saveTo(store)
     }
 
     override fun onChange(function: (PresetItem) -> Unit) = eventChange.listen(function)
 
     override var value: PresetItem
-        get() = _value
+        get() = loadedValue
         set(value) = value(value)
 
     override fun toString() = "${super.toString()} key:$key value:$value"
-    override fun fireChange() = eventChange.fire(_value)
+    override fun fireChange() = eventChange.fire(loadedValue)
 }
