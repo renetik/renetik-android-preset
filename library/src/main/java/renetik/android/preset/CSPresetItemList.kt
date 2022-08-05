e823392c810070fc402a812ec122177b8dec7c22
Rene Dohan <dohan.rene@gmail.com>
2022-08-04 23:02:31 -0500

    updateModuleToGithub

--- library/src/main/java/renetik/android/preset/CSPresetItemList.kt
+++ library/src/main/java/renetik/android/preset/CSPresetItemList.kt
@@ -1,14 +1,16 @@
 package renetik.android.preset
 
-import renetik.android.core.java.util.currentTime
+import java.lang.System.nanoTime
 
 interface CSPresetItemList<PresetItem : CSPresetItem> {
     companion object
+
     val defaultItems: List<PresetItem>
     val userItems: List<PresetItem>
     fun add(item: PresetItem)
     fun remove(item: PresetItem)
     fun createPresetItem(title: String, isDefault: Boolean,
-                         id: String = "$currentTime"): PresetItem
+                         id: String = "${nanoTime()}"): PresetItem
+
     fun reload()
 }
