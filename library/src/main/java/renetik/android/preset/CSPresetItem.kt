688344648fc33587dcd3f8d877cf9879ca3845d4
Rene Dohan - Renetik <dohan.rene@gmail.com>
2023-01-04 13:20:19 +0400

    ...

--- library/src/main/java/renetik/android/preset/CSPresetItem.kt
+++ library/src/main/java/renetik/android/preset/CSPresetItem.kt
@@ -4,10 +4,14 @@ import renetik.android.core.lang.CSHasId
 import renetik.android.core.lang.CSHasIndex
 import renetik.android.preset.property.CSPresetKeyData
 import renetik.android.store.CSStore
+import renetik.android.store.property.CSStoreProperty
 
 interface CSPresetItem : CSHasId, CSHasIndex {
     companion object
 
     val store: CSStore
+
+    val title: CSStoreProperty<String>
+
     fun save(properties: Iterable<CSPresetKeyData>)
 }
