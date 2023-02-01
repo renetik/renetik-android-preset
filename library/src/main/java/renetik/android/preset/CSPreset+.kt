eb0b2d3787527a8a0e9b7cf1769567143a13a572
Rene Dohan <dohan.rene@gmail.com>
2023-02-01 13:58:30 +0400

    ...

--- library/src/main/java/renetik/android/preset/CSPreset+.kt
+++ library/src/main/java/renetik/android/preset/CSPreset+.kt
@@ -1,11 +1,27 @@
 package renetik.android.preset
 
+import renetik.android.core.lang.Func
 import renetik.android.event.property.CSProperty
 import renetik.android.event.property.connect
+import renetik.android.event.registration.CSRegistration
+import renetik.android.event.registration.pause
 import renetik.android.event.registration.register
+import renetik.android.event.registration.resume
 
 typealias Preset = CSPreset<*, out CSPresetDataList<*>>
 
 fun <T : Preset> T.followStoreIf(property: CSProperty<Boolean>) = apply {
     register(isFollowStore.connect(property))
+}
+
+fun <T : Preset> T.onChange(
+    vararg registrations: CSRegistration,
+    function: Func
+): CSRegistration {
+    val onBeforeChange = onBeforeChange { registrations.pause() }
+    val onChange = onChange {
+        function()
+        registrations.resume()
+    }
+    return CSRegistration(onBeforeChange, onChange)
 }
