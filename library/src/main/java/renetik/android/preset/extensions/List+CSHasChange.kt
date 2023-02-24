ed74a8ad01808b6cff5733aefd30ea483cd15358
Rene Dohan <dohan.rene@gmail.com>
2023-02-24 14:09:35 +0100

    ...

--- library/src/main/java/renetik/android/preset/extensions/List+CSHasChange.kt
+++ library/src/main/java/renetik/android/preset/extensions/List+CSHasChange.kt
@@ -7,6 +7,21 @@ import renetik.android.event.registration.CSRegistration
 import renetik.android.event.registration.CSRegistrationsList
 import renetik.android.preset.Preset
 
+inline fun <T : CSHasChange<*>> List<T>.onChange(
+    preset: Preset, crossinline function: Func
+): CSRegistration {
+    val registrations = CSRegistrationsList(this)
+    forEach { registrations.register(it.onChange { function() }) }
+    return CSRegistration(
+        registrations,
+        preset.onBeforeChange { registrations.pause() },
+        preset.onChange {
+            function()
+            registrations.resume()
+        }
+    )
+}
+
 inline fun <T : CSHasChange<*>> List<T>.onChangeLater(
     preset: Preset, crossinline function: Func
 ): CSRegistration {
