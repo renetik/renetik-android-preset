68f2ab2e0ccd95e66b57757d7fe5327b495ec2a5
Rene Dohan <dohan.rene@gmail.com>
2023-04-19 11:53:51 +0200

    ...

--- library/src/main/java/renetik/android/preset/CSPresetDataList.kt
+++ library/src/main/java/renetik/android/preset/CSPresetDataList.kt
@@ -1,19 +1,11 @@
 package renetik.android.preset
 
-import java.lang.System.nanoTime
-
 interface CSPresetDataList<PresetItem : CSPresetItem> {
     companion object
 
     val defaultItems: List<PresetItem>
     val userItems: List<PresetItem>
     fun remove(item: PresetItem)
-
-    fun createItem(
-        id: String = "${nanoTime()}",
-        title: String,
-        isDefault: Boolean
-    ): PresetItem
-
+    fun createItem(title: String, isDefault: Boolean): PresetItem
     fun reload()
 }
