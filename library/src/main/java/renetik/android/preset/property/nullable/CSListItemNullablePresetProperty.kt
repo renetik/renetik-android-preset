a40efb8b81e409b19e897399245c4c0af7b44da1
Rene Dohan <dohan.rene@email.com>
2024-05-11 21:11:23 +0800

    Fix store context property

--- library/src/main/java/renetik/android/preset/property/nullable/CSListItemNullablePresetProperty.kt
+++ library/src/main/java/renetik/android/preset/property/nullable/CSListItemNullablePresetProperty.kt
@@ -1,11 +1,11 @@
 package renetik.android.preset.property.nullable
 
+import renetik.android.core.kotlin.toId
 import renetik.android.event.common.CSHasRegistrationsHasDestruct
 import renetik.android.event.property.CSListValuesProperty
+import renetik.android.json.obj.getValue
 import renetik.android.preset.CSPreset
 import renetik.android.store.CSStore
-import renetik.android.core.kotlin.toId
-import renetik.android.json.obj.getValue
 
 class CSListItemNullablePresetProperty<T>(
     parent: CSHasRegistrationsHasDestruct,
@@ -13,8 +13,8 @@ class CSListItemNullablePresetProperty<T>(
     key: String,
     val getValues: () -> List<T>,
     val getDefault: () -> T,
-    onChange: ((value: T?) -> Unit)? = null)
-    : CSNullablePresetProperty<T>(parent, preset, key, onChange),
+    onChange: ((value: T?) -> Unit)? = null
+) : CSNullablePresetProperty<T>(parent, preset, key, onChange),
     CSListValuesProperty<T?> {
 
     constructor(
@@ -22,7 +22,8 @@ class CSListItemNullablePresetProperty<T>(
         preset: CSPreset<*, *>, key: String,
         values: List<T>, default: T, onChange: ((value: T?) -> Unit)? = null
     ) : this(parent, preset, key, getValues = { values },
-        getDefault = { default }, onChange)
+        getDefault = { default }, onChange
+    )
 
     override val values: List<T> get() = getValues()
     override val default: T get() = getDefault()
