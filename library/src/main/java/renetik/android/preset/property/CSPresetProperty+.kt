22b42364cddfc066ad2e99b324149655d944a0e3
Rene Dohan <dohan.rene@email.com>
2024-05-15 01:24:40 +0800

    ...

--- library/src/main/java/renetik/android/preset/property/CSPresetProperty+.kt
+++ library/src/main/java/renetik/android/preset/property/CSPresetProperty+.kt
@@ -6,6 +6,14 @@ fun <T : CSStoreProperty<Int>> T.max(value: Int) = apply {
     filter = { it?.coerceAtMost(value) }
 }
 
+fun <T : CSStoreProperty<Float?>> T.max(value: Float?) = apply {
+    value?.let { filter = { it?.coerceAtMost(value) } }
+}
+
 fun <T : CSStoreProperty<Int>> T.min(value: Int) = apply {
     filter = { it?.coerceAtLeast(value) }
+}
+
+fun <T : CSStoreProperty<Float?>> T.min(value: Float?) = apply {
+    value?.let { filter = { it?.coerceAtLeast(value) } }
 }
