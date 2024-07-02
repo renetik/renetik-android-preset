0bbf1a3334cc0516c3d26ef11cdae51475f6f308
Renetik <dohan.rene@gmail.com>
2024-07-02 17:09:37 +0800

    ...

--- library/src/main/java/renetik/android/preset/CSPresetItemList+.kt
+++ library/src/main/java/renetik/android/preset/CSPresetItemList+.kt
@@ -3,7 +3,7 @@ package renetik.android.preset
 import renetik.android.core.kotlin.collections.list
 import renetik.android.core.kotlin.collections.putAll
 
-val <PresetItem : CSPresetItem> CSPresetDataList<PresetItem>.items
+val <PresetItem : CSPresetItem> CSPresetDataList<PresetItem>.items: List<PresetItem>
     get() = list(defaultItems).putAll(userItems)
 
 val CSPresetDataList<*>.count get() = defaultItems.size + userItems.size
