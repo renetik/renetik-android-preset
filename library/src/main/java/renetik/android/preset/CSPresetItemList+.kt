291fbf69cc04435d641c7a108de102d3118afe7f
Renetik <dohan.rene@gmail.com>
2024-07-02 17:13:47 +0800

    ...

--- library/src/main/java/renetik/android/preset/CSPresetItemList+.kt
+++ library/src/main/java/renetik/android/preset/CSPresetItemList+.kt
@@ -3,6 +3,8 @@ package renetik.android.preset
 import renetik.android.core.kotlin.collections.list
 import renetik.android.core.kotlin.collections.putAll
 
+// TODO: This is little performance issue as this is creating large list in case of
+//  controller presets multiple times repeatedly
 val <PresetItem : CSPresetItem> CSPresetDataList<PresetItem>.items: List<PresetItem>
     get() = list(defaultItems).putAll(userItems)
 
