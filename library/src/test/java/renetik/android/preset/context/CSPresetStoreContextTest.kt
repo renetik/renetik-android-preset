00b4c166ba9108903dced0f08750d0dbaebbae7e
Rene Dohan <dohan.rene@email.com>
2024-10-24 16:41:31 +0800

    ...

--- library/src/test/java/renetik/android/preset/context/CSPresetStoreContextTest.kt
+++ library/src/test/java/renetik/android/preset/context/CSPresetStoreContextTest.kt
@@ -5,7 +5,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.test.StandardTestDispatcher
-import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
@@ -19,20 +18,13 @@ import renetik.android.core.extensions.content.createTempFile
 import renetik.android.core.java.io.readString
 import renetik.android.core.lang.CSEnvironment
 import renetik.android.event.common.CSModel
-import renetik.android.json.toJson
 import renetik.android.preset.CSPreset
-import renetik.android.preset.CSPreset.Companion.CSPreset
-import renetik.android.preset.extensions.property
 import renetik.android.preset.init
 import renetik.android.preset.model.CSPresetTestPresetItemList
 import renetik.android.preset.model.ClearPresetItemId
 import renetik.android.preset.model.NotFoundPresetItem
 import renetik.android.preset.model.manageItems
-import renetik.android.preset.property.max
-import renetik.android.store.extensions.reload
 import renetik.android.store.type.CSFileJsonStore
-import renetik.android.store.type.CSJsonObjectStore
-import renetik.android.testing.CSAssert.assert
 import renetik.android.testing.CSAssert.assertContains
 import kotlin.time.Duration.Companion.seconds
 
@@ -79,91 +71,5 @@ class CSPresetStoreContextTest {
         context.destructClear()
         checkStoreFor("""""")
     }
-
-    @Test
-    fun childPresetPropertyPresetReload() = runTest {
-        val parent = CSModel()
-        val presetList = CSPresetTestPresetItemList(ClearPresetItemId)
-        val preset = CSPreset(
-            parent, CSJsonObjectStore(), "preset", presetList, ::NotFoundPresetItem
-        ).manageItems().init()
-        val childPreset = CSPreset(
-            parent, preset, "childPreset", presetList, ::NotFoundPresetItem
-        ).manageItems().init()
-        val childPresetProperty = childPreset.property(parent, "preset1 property", 5)
-        assertEquals(5, childPresetProperty.value)
-
-        childPresetProperty.value = 10
-        assertEquals(10, childPresetProperty.value)
-
-        preset.reload()
-        advanceUntilIdle()
-        assertEquals(5, childPresetProperty.value)
-    }
-
-    @Test
-    fun childPresetPropertySaveAsNewItemPresetReload() = runTest {
-        val parent = CSModel()
-        val preset = CSPreset(
-            parent, CSJsonObjectStore(), "preset",
-            CSPresetTestPresetItemList(clearItemId = "clear preset item"),
-            ::NotFoundPresetItem
-        ).manageItems().init()
-        val defaultItemId = "clear childPreset item"
-        val childPreset = CSPreset(
-            parent, preset, "childPreset",
-            CSPresetTestPresetItemList(clearItemId = defaultItemId),
-            ::NotFoundPresetItem, defaultItemId
-        ).manageItems().init()
-        val childPresetProperty = childPreset.property(parent, "childPresetProperty", 5)
-        assertEquals(5, childPresetProperty.value)
-        advanceUntilIdle()
-        assertContains(
-            preset.store.data.toJson(),
-            """"childPreset preset current":"clear childPreset item"""",
-            """"childPreset preset store":{"childPresetProperty":5}}""",
-        )
-
-        childPresetProperty.value = 10
-        assertEquals(10, childPresetProperty.value)
-        val item = childPreset.list.createItem("childPresetProperty item", isDefault = true)
-        childPreset.saveAsNew(item)
-        advanceUntilIdle()
-        assertContains(
-            preset.store.data.toJson(),
-            """"childPreset preset current":"childPresetProperty item"""",
-            """"childPreset preset store":{"childPresetProperty":10}}"""
-        )
-        preset.reload()
-        advanceUntilIdle()
-        assert(expected = 2, preset.store.data.size)
-        assert(
-            expected = "clear childPreset item",
-            actual = preset.store.data["childPreset preset current"]
-        )
-        assert(
-            expected = """{"childPresetProperty":5}""",
-            actual = preset.store.data["childPreset preset store"]?.toJson()
-        )
-        assertEquals(5, childPresetProperty.value)
-    }
-
-    @Test
-    fun presetPropertyMax() {
-        val parent = CSModel()
-        val presetList = CSPresetTestPresetItemList(ClearPresetItemId)
-
-        val store = CSJsonObjectStore()
-        store.reload("""{"preset preset store":{"key":10}}""")
-        val preset = CSPreset(
-            parent, store, "preset", presetList, ::NotFoundPresetItem
-        ).manageItems().init()
-        val property = preset.property(parent, "key", 5)
-        assertEquals(10, property.value)
-
-        val propertyMax = preset.property(parent, "key", 5).max(7)
-        assertEquals(7, propertyMax.value)
-    }
-
 }
 
