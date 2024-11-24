97c2b5a551ed54595b47d30e84abfb8c000bc42f
Rene Dohan <dohan.rene@email.com>
2024-11-24 18:20:32 +0800

    ...

--- library/src/test/java/renetik/android/preset/context/CSPresetStoreContextTest.kt
+++ library/src/test/java/renetik/android/preset/context/CSPresetStoreContextTest.kt
@@ -18,6 +18,7 @@ import renetik.android.core.extensions.content.createTempFile
 import renetik.android.core.java.io.readString
 import renetik.android.core.lang.CSEnvironment.app
 import renetik.android.event.common.CSModel
+import renetik.android.event.common.destruct
 import renetik.android.preset.CSPreset
 import renetik.android.preset.init
 import renetik.android.preset.model.CSPresetTestPresetItemList
@@ -25,6 +26,7 @@ import renetik.android.preset.model.ClearPresetItemId
 import renetik.android.preset.model.NotFoundPresetItem
 import renetik.android.preset.model.manageItems
 import renetik.android.store.type.CSFileJsonStore
+import renetik.android.testing.CSAssert.assert
 import renetik.android.testing.CSAssert.assertContains
 import kotlin.time.Duration.Companion.seconds
 
@@ -38,39 +40,48 @@ class CSPresetStoreContextTest {
     @After
     fun tearDown() = Dispatchers.resetMain()
 
-    @Test
-    fun presetPropertyPresetReload() = runTest {
-        val parent = CSModel()
-        val presetList = CSPresetTestPresetItemList(ClearPresetItemId)
-        val testFile = app.createTempFile()
-        val store = CSFileJsonStore(
-            testFile, isJsonPretty = false, isImmediateWrite = false
-        )
-        val preset = CSPreset(
-            parent, store, "preset1", presetList, ::NotFoundPresetItem
-        ).manageItems().init()
-        val contextParent = CSModel(parent)
-        val context = PresetStoreContext(
-            contextParent, id = "contextId", preset, presetId = "contextKey"
-        )
-        val property = context.property("property", 5)
-        fun checkStoreFor(content: String) {
-            runBlocking { delay(2.seconds) }
-            assertContains(testFile.readString()!!, content)
-        }
-        assertEquals(5, property.value)
-//        checkStoreFor("""{"contextKey property":5}""")
+    private val parent = CSModel()
+    private val presetList = CSPresetTestPresetItemList(ClearPresetItemId)
+    private val testFile = app.createTempFile()
+    private val store = CSFileJsonStore(
+        testFile, isJsonPretty = false, isImmediateWrite = false
+    )
+    private val preset = CSPreset(
+        parent, store, "preset1", presetList, ::NotFoundPresetItem
+    ).manageItems().init()
+    private val contextParent = CSModel(parent)
+    private val context = PresetStoreContext(
+        contextParent, id = "contextId", preset, presetId = "contextKey"
+    )
 
+    private fun checkStoreFor(content: String) {
+        runBlocking { delay(2.seconds) }
+        assertContains(actual = testFile.readString()!!, content)
+    }
+
+    @Test
+    fun presetReload() = runTest {
+        val property = context.property("property", 5)
+        assertEquals(5, property.value)
+        checkStoreFor(""""preset1 preset store":{}""")
         property.value = 10
         assertEquals(10, property.value)
-        checkStoreFor("""{"contextKey property":10}""")
-
+        checkStoreFor(""""preset1 preset store":{"contextKey property":10}""")
         preset.reload()
         assertEquals(5, property.value)
-//        checkStoreFor("""{"contextKey property":5}""")
+        checkStoreFor(""""preset1 preset store":{}""")
+    }
 
-        context.destructClear()
-        checkStoreFor("""""")
+    @Test
+    fun contextClear() = runTest {
+        val property = context.property("property", 5)
+        property.value = 10
+        assertEquals(10, property.value)
+        runBlocking { delay(2.seconds) }
+        checkStoreFor("""{"contextKey property":10}""")
+        context.clear()
+        contextParent.destruct()
+        checkStoreFor(""""preset1 preset store":{}""")
     }
 }
 
