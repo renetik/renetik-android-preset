57c204d6c757e72ed0665773dda7f1f6bf40f29a
Rene Dohan <dohan.rene@email.com>
2024-12-20 19:51:29 +0800

    ...

--- library/src/main/java/renetik/android/preset/property/CSPresetEventProperty+.kt
+++ library/src/main/java/renetik/android/preset/property/CSPresetEventProperty+.kt
@@ -4,8 +4,6 @@ import renetik.android.event.property.CSProperty
 import renetik.android.event.property.connect
 import renetik.android.event.registration.CSRegistration
 
-//val CSPresetProperty<*>.store: CSStore get() = preset.store
-
 fun <T> CSPresetProperty<T>.followPresetIf(
     property: CSProperty<Boolean>
 ): CSRegistration = isFollowPreset.connect(property)
