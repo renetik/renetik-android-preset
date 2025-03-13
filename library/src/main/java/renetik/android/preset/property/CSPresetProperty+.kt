8a194bc0b2c4aaafa80c43aa6f5616227d0049b9
Rene Dohan <dohan.rene@gmail.com>
2025-03-13 21:42:40 +0800

    Coerce Tempo

--- library/src/main/java/renetik/android/preset/property/CSPresetProperty+.kt
+++ library/src/main/java/renetik/android/preset/property/CSPresetProperty+.kt
@@ -1,30 +1,23 @@
 package renetik.android.preset.property
 
+import renetik.android.event.property.CSProperty
 import renetik.android.event.property.computedFrom
-import renetik.android.store.property.CSStoreProperty
 
-fun <T : CSStoreProperty<Int>> T.max(value: Int) =
+fun <T : CSProperty<Int>> T.coerce(range: ClosedRange<Int>) =
+    computedFrom(from = { it.coerceIn(range) })
+
+fun <T : CSProperty<Int>> T.max(value: Int) =
     computedFrom(from = { it.coerceAtMost(value) })
-//    apply { filter = { it?.coerceAtMost(value) } }
 
-fun <T : CSStoreProperty<Float?>> T.max(value: Float?) =
+fun <T : CSProperty<Float?>> T.max(value: Float?) =
     computedFrom(from = { from ->
         value?.let { from?.coerceAtMost(value) } ?: from
     })
-//    apply {
-//    value?.let { filter = { it?.coerceAtMost(value) } }
-//}
 
-fun <T : CSStoreProperty<Int>> T.min(value: Int) =
+fun <T : CSProperty<Int>> T.min(value: Int) =
     computedFrom(from = { it.coerceAtLeast(value) })
-//    apply {
-//    filter = { it?.coerceAtLeast(value) }
-//}
 
-fun <T : CSStoreProperty<Float?>> T.min(value: Float?) =
+fun <T : CSProperty<Float?>> T.min(value: Float?) =
     computedFrom(from = { from ->
         value?.let { from?.coerceAtLeast(value) } ?: from
     })
-//    apply {
-//    value?.let { filter = { it?.coerceAtLeast(value) } }
-//}
